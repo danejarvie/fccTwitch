ef11dadd8b87b587e295c088e9429b8d62ad5a4e
Dane <danejarvie@gmail.com>
2018-01-08 21:47:57 +1300

    Found Twitch embed

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,47 @@
+
+var channelList = [ "freecodecamp", "nl_Kripp", "HSdogdog", "itsHafu", "BobRoss", "sentdex", "DisguisedToastHS", "Tizzleroo"];
+var twitchClientID = "nocu78lvcal2ijvy6slyc2soz3qqmo";
+//var channelApi = "https://wind-bow.gomix.me/twitch-api/channels/"; //freecodecamp workaround
+var channelApi = "https://api.twitch.tv/helix/users?login="; //latest url
+//var channelApi = "https://api.twitch.tv/kraken/channels/twitch"; //v5 url
+//var streamApi = "https://wind-bow.gomix.me/twitch-api/streams/";
+//var streamApi = "https://api.twitch.tv/kraken/streams/";
+var streamApi = "https://api.twitch.tv/helix/streams?user_id=81119098";
+var jsonpSuffix = "?callback=?";
+var channelHTML = "";
+var myHeaders = new Headers({
+    'Content-type': 'application/json',
+    'Client-ID': twitchClientID
+});
+var myInit = { method: 'GET',
+               headers: myHeaders
+               //mode: 'cors',
+               //cache: 'default' 
+            };
+
+
 angular.module("app", [])
-    .controller("TodoController", function() {
-        var todoList = this;
+    .controller("app", function($scope) {
+        
+        $scope.liveChannels = [];
+        $scope.offlineChannels = [];
+        /*
+        channelList.forEach(function(value) {
+            console.log(value);
+            fetch(channelApi + value, myInit)
+                .then(function(response){
 
-        todoList.listItems = [
-            { text:"learn Angularjs", done: true },
-            { text:"build Todo list app", done: false }
-        ];
+                });
 
-        todoList.addItem = function() {
-            var listText = todoList.todoText;
-            if(listText && /\S/.test(listText)) {
-                todoList.listItems.push({text: listText, done: false});
-            }
-            todoList.todoText = '';
-        };
+            
+        });*/
+        fetch(streamApi, myInit)
+                .then(function(response){
 
-        todoList.removeItem = function(todo) {
-            todoList.listItems = todoList.listItems.filter(x => x.text != todo.text);
-        };
+                });
+
+
+        
 
 
     });
